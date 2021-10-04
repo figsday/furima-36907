097b130add10579b0dba47c6933782239506b818
figsday <ayaneinai1@gmail.com>
2021-10-04 15:28:11 +0900

    ユーザーモデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  has_many :histories, dependent: :destroy
+  has_many :items,dependent: :destroy
+     
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
