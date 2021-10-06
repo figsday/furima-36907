faf32da248a3a000cdb88e1f0eddc7b41200cc61
figsday <ayaneinai1@gmail.com>
2021-10-06 18:35:43 +0900

    商品出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,13 +10,18 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :shipping_fee
 
-  validates :user,             presence: true
-  validates :item_name,        presence: true
-  validates :description,      presence: true
-  validates :price,            presence: true
+  #validates :user,             presence: true, foreign_key: true
+  validates :item_name,        presence: true, length: { maximum: 40 }
+  validates :description,      presence: true, length: { maximum: 1000 }
   validates :category_id,      presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :condition_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_fee_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :region_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :delivery_time_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :image,            presence: true
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: { message: "can't be blank" }
+  end
 end
